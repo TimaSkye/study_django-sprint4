2ab75c69daf8eefc65d65c41d5843b3278e549ba
Тимур Даянов <tima.skye@yandex.ru>
2025-05-02 19:22:56 +0500

    Обработчики ошибок 403, 404, 500

--- blogicum/core/views.py
+++ blogicum/core/views.py
@@ -9,5 +9,5 @@ def csrf_failure(request, reason=''):
     return render(request, 'pages/403csrf.html', status=403)
 
 
-def internal_server_error(request, exception):
+def internal_server_error(request):
     return render(request, 'pages/500.html', status=500)
