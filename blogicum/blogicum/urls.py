e9ae912a7bac1d29a6f08ddc755f84e7e3ea04bd
Тимур Даянов <tima.skye@yandex.ru>
2025-05-08 13:34:19 +0500

    Обработчики ошибок 403, 404, 500

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -2,11 +2,12 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
-handler404 = 'core.views.page_not_found'
-handler500 = 'core.views.internal_server_error'
+handler404 = 'pages.views.page_not_found'
+handler500 = 'pages.views.internal_server_error'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', include('django.contrib.auth.urls')),
     path('', include('blog.urls')),
     path('pages/', include('pages.urls')),
 ]
--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -9,3 +9,18 @@ def about(request):
 def rules(request):
     """Фунцкия рендера страницы правил."""
     return render(request, 'pages/rules.html')
+
+
+def page_not_found(request, exception):
+    """Функция рендера кастомной страницы ошибки 404."""
+    return render(request, 'pages/404.html', status=404)
+
+
+def csrf_failure(request, reason=''):
+    """Функция рендера кастомной страницы ошибки 403."""
+    return render(request, 'pages/403csrf.html', status=403)
+
+
+def internal_server_error(request):
+    """Функция рендера кастомной страницы ошибки 500."""
+    return render(request, 'pages/500.html', status=500)
