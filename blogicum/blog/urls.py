f76f855c0d9f328cd839c6a2cb6720f0ab7bef9d
Тимур Даянов <tima.skye@yandex.ru>
2025-05-08 16:07:02 +0500

    работа с пользователями и профилями пользователей

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -5,14 +5,14 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('create_post/', views.create_post, name='create_post'),
     path('posts/<int:post_id>/', views.post_detail, name='post_detail'),
     path(
         'category/<slug:category_slug>/',
         views.category_posts,
         name='category_posts'
     ),
-    path('profile/edit_profile/', views.edit_profile,
-         name='edit_profile'),
-    path('create_post/', views.create_post, name='create_post'),
     path('profile/<str:username>/', views.profile, name='profile'),
+    path('edit_profile/', views.edit_profile,
+         name='edit_profile'),
 ]
